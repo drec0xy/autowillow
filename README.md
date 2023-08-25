fda2edb3b775e8e257cbe3e43949443a4dce807e
drec0xy <timmydchoune@gmail.com>
2023-08-25 09:06:46 +0100

    🚀

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flask_Demo_Rest# autowillow_backend
+# autowillow_backend
+# autowillow_backend
+# autowillow_backend
---
+++ config.py
@@ -0,0 +1,33 @@
+"""Flask configuration variables."""
+from os import environ, path
+
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+class Config:
+    """Set Flask configuration from .env file."""
+
+    # General Config
+    SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+
+    # Configs for email server
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USERNAME = environ.get("EMAIL_HOST_USER")
+    MAIL_PASSWORD = environ.get("EMAIL_HOST_PASSWORD")
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    def __repr__(self) -> str:
+        pass
